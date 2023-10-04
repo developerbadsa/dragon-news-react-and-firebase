ee08f938825dbfa0202a0363b52e48b8a1bfeeee
Rahim_Badsa <developerbadsa@gmail.com>
2023-10-04 13:55:13 +0600

    added header content to this components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
     return ( 
         <div className="text-center my-8">
             <img className="mx-auto" src="./../../../public/images/logo.png" alt="" />
-            <h3>Journalism Without Fear or Favour</h3>
-            <p>Sunday, November 27, 2022</p>
+            <h3 className="text-lg font-normal text-slate-500">Journalism Without Fear or Favour</h3>
+            <p>Sunday, <span className="text-slate-500 font-medium text-xl">November 27, 2022</span></p>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      poppins: {
+        custom: ['Poppins', 'sans-serif'], 
+      },
+    },
   },
   plugins: [require("daisyui")]
 }
-
