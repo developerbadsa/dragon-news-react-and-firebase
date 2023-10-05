313cca5490f1d226abfdd1d5ba0b12f442a8965b
Rahim_Badsa <developerbadsa@gmail.com>
2023-10-05 10:35:02 +0600

    marquee installed and using on lastest news section

--- src/Components/LastestNews/LastestNews.jsx
+++ src/Components/LastestNews/LastestNews.jsx
@@ -1,9 +1,10 @@
+import Marquee from "react-fast-marquee";
 
 
 const LastestNews = () => {
     return (
         <div className="flex items-center gap-4">
-            <button className="btn bg-[#D72050] hover:bg-[#D72050] rounded-none text-white">Latest</button> <span>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</span>
+            <button className="btn bg-[#D72050] hover:bg-[#D72050] rounded-none text-white">Latest</button> <Marquee><span>Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</span></Marquee>
         </div>
     );
 };
