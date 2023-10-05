95a2f9531f81cab9b580a22bd2b3eba641560984
Rahim_Badsa <developerbadsa@gmail.com>
2023-10-05 11:21:31 +0600

    added json file to public folder and fetch that for show home page news

--- src/Pages/Home/HomeNews/HomeNews.jsx
+++ src/Pages/Home/HomeNews/HomeNews.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
 
 const HomeNews = () => {
+    const [News, setNews] = useState([])
+    useEffect(
+    ()=>{
+
+        fetch('/public/news.json')
+        .then(res=>res.json())
+        .then(data=>setNews(data))
+
+    }
+        
+        ,[])
+
+console.log(News);
+
     return (
         <div>
-            
+            <h3 className="text-xl font-semibold text-slate-600 my-8">All Caterogy</h3>
+
         </div>
     );
 };
