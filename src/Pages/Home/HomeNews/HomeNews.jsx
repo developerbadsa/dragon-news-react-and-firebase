5552ccaa095fa22f2d7067c9d19d40ccac1055bc
Rahim_Badsa <developerbadsa@gmail.com>
2023-10-05 12:02:19 +0600

    making card of home page news

--- src/Pages/Home/HomeNews/HomeNews.jsx
+++ src/Pages/Home/HomeNews/HomeNews.jsx
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react";
+import SingleNews from "./SingleNews";
 
 
 const HomeNews = () => {
     const [News, setNews] = useState([])
     useEffect(
-    ()=>{
+        () => {
 
-        fetch('/public/news.json')
-        .then(res=>res.json())
-        .then(data=>setNews(data))
+            fetch('/public/news.json')
+                .then(res => res.json())
+                .then(data => setNews(data))
 
-    }
-        
-        ,[])
+        }
 
-console.log(News);
+        , [])
 
     return (
         <div>
             <h3 className="text-xl font-semibold text-slate-600 my-8">All Caterogy</h3>
-
+            <div>
+                {
+                    News.map(news => <SingleNews key={news._id} news={news} ></SingleNews>)
+                }
+            </div>
         </div>
     );
 };
